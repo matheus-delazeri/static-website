e922b6121be3d628b8128b2915e34f4ebc4e88c4
matheus <matheusdelazeris@gmail.com>
2021-07-02 19:51:39 -0300

    added style.css

--- index.php
+++ index.php
@@ -1,3 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Clara</title>
+</head>
+<body>
 <?php
-echo "<h3>Hello World!</h3>";
+echo "<h3>Eh o grêmio!</h3>";
 ?>
+<center>
+<input id="msgBtn" type="submit" class="btn" onclick="showMessage()" value="Clique aqui!">
+<p id="message" style="display:none">Parabéns Clara, tudo de bom! Até mais :)</p>
+</center>
+
+<script type="text/javascript">
+
+function showMessage(){
+    var message = document.getElementById("message");
+    var msgBtn = document.getElementById("msgBtn");
+    message.style.display = "block";
+    msgBtn.style.display = "none";
+}
+
+</script>
+</body>
+</html>
